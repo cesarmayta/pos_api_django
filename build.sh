a2097181ddd539a9f1947b0629077d8ba4b3b28d
César Mayta <cesarmayta@gmail.com>
2022-11-29 12:02:13 -0500

    deploy 5

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 #python manage.py createsuperuser --no-input --username
-python manage.py createsuperuser2 --username admin --password Tecsup2022
+#python manage.py createsuperuser2 --username admin --password Tecsup2022
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'cmayta@tecsup.edu.pe', 'Tecsup2022')" | python manage.py shell
